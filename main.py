2847af85df7b4f0f804fa7f0b481970ca2a0fb30
kawesomekhan <70611864+kawesomekhan@users.noreply.github.com>
2020-09-12 02:33:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 myInd = ind.Indoors()
 results_df = myInd.calc_n_max_series(2, 100, 1.0)
 fig = px.line(results_df, x="Maximum Exposure Time (hours)", y="Maximum Occupancy",
