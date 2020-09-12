31c9a261407f05718422ec969badb8d96dffc1d3
kawesomekhan <70611864+kawesomekhan@users.noreply.github.com>
2020-09-12 02:25:11 -0500

    Added Files

---
+++ indoors.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy
+
+
+class Indoors:
+    physical_params = []
+    physio_params = []
+    disease_params = []
+    prec_params = []
+
+    def __init__(self):
+        self.set_default_params()
+
+    def calc_n_max(self, exp_time):
+        # Physical Parameters
+        floor_area = self.physical_params[0]  # ft2
+        mean_ceiling_height = self.physical_params[1]  # ft
+        air_exch_rate = self.physical_params[2]  # /hr
+        primary_outdoor_air_fraction = self.physical_params[3]  # no units
+        aerosol_filtration_eff = self.physical_params[4]  # no units
+
+        # Physiological Parameters
+        breathing_flow_rate = self.physio_params[0]  # m3 / hr
+        aerosol_radius = self.physio_params[1]
+
+        # Disease Parameters
+        exhaled_air_inf = self.disease_params[0]  # infection quanta/m3
+        viral_deact_rate = self.disease_params[1]  # /hr
+
+        # Precautionary Parameters
+        mask_passage_prob = self.prec_params[0]  # no units
+        risk_tolerance = self.prec_params[1]  # no units
+
+        # Calculation
+        mean_ceiling_height_m = mean_ceiling_height * 0.3048
+        room_vol = floor_area * mean_ceiling_height  # ft3
+        room_vol_m = 0.0283168 * room_vol  # m3
+
+        fresh_rate = room_vol * air_exch_rate / 60  # ft3/min
+
+        recirc_rate = fresh_rate * (1/primary_outdoor_air_fraction - 1)  # ft3/min
+
+        air_filt_rate = aerosol_filtration_eff * recirc_rate * 60 / room_vol  # /hr
+
+        sett_speed = 3 * (aerosol_radius / 5) ** 2  # mm/s
+        sett_speed = sett_speed * 60 * 60 / 1000  # m/hr
+
+        conc_relax_rate = air_exch_rate + air_filt_rate + viral_deact_rate + sett_speed / mean_ceiling_height_m  # /hr
+
+        airb_trans_rate = ((breathing_flow_rate * mask_passage_prob) ** 2) * exhaled_air_inf / (room_vol_m * conc_relax_rate)
+
+        n_max = 1 + (risk_tolerance * (1 + 1/(conc_relax_rate * exp_time)) / (airb_trans_rate * exp_time))
+        return n_max
+
+    def calc_n_max_series(self, t_min, t_max, t_step):
+        df = pd.DataFrame(columns=['Maximum Exposure Time (hours)', 'Maximum Occupancy'])
+        for exp_time in numpy.arange(t_min, t_max, t_step):
+            n_max = self.calc_n_max(exp_time)
+            df = df.append(pd.DataFrame({'Maximum Exposure Time (hours)': [exp_time], 'Maximum Occupancy': [n_max]}))
+
+        return df
+
+    def set_default_params(self):
+        # Physical Parameters
+        floor_area = 900  # ft2
+        mean_ceiling_height = 12  # ft
+        air_exchange_rate = 3  # /hr (air changes per hour (ACH))
+        primary_outdoor_air_fraction = 0.2  # 1.0 = natural ventilation
+        aerosol_filtration_eff = 0  # >0.9997 HEPA, =0.2-0.9 MERVs, =0 no filter
+        self.physical_params = [floor_area, mean_ceiling_height, air_exchange_rate, primary_outdoor_air_fraction,
+                                aerosol_filtration_eff]
+
+        # Physiological Parameters
+        breathing_flow_rate = 0.5  # m3/hr
+        aerosol_radius = 2  # micrometers
+        self.physio_params = [breathing_flow_rate, aerosol_radius]
+
+        # Disease Parameters
+        exhaled_air_inf = 30  # infection quanta/m3
+        viral_deact_rate = 0.3  # /hr
+        self.disease_params = [exhaled_air_inf, viral_deact_rate]
+
+        # Precautionary Parameters
+        mask_passage_prob = 0.1  # 1 = no masks, ~0.1 cloth, <0.05 N95
+        risk_tolerance = 0.1  # expected transmissions per infector
+        self.prec_params = [mask_passage_prob, risk_tolerance]
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,173 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import indoors as ind
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+myInd = ind.Indoors()
+results_df = myInd.calc_n_max_series(2, 100, 1.0)
+fig = px.line(results_df, x="Maximum Exposure Time (hours)", y="Maximum Occupancy",
+              title="Maximum Occupancy & Exposure Time")
+
+ventilation_types = [
+    {'label': "Bedroom, closed windows", 'value': 0.34},
+    {'label': "Mechanical Ventilation", 'value': 6},
+    {'label': "Restaurant", 'value': 17.5},
+    {'label': "Hospital", 'value': 18},
+    {'label': "Laboratory", 'value': 17.5},
+    {'label': "Toxic Laboratory", 'value': 25},
+]
+
+filter_types = [
+    {'label': "None", 'value': 0},
+    {'label': "HEPA", 'value': 0.9997},
+    {'label': "MERVs", 'value': 0.5},
+]
+
+exertion_types = [
+    {'label': "Resting", 'value': 0.49},
+    {'label': "Standing", 'value': 0.54},
+    {'label': "Light Exercise", 'value': 1.38},
+    {'label': "Moderate Exercise", 'value': 2.35},
+    {'label': "Heavy Exercise", 'value': 3.30},
+]
+
+expiratory_types = [
+    {'label': "Breathing (nose)", 'value': 1.1},
+    {'label': "Breathing (nose-nose)", 'value': 8.8},
+    {'label': "Breathing (deep-fast)", 'value': 4.2},
+    {'label': "Breathing (fast-deep)", 'value': 8.5},
+    {'label': "Speaking (quiet speech)", 'value': 29},
+    {'label': "Speaking (whispered counting)", 'value': 37},
+    {'label': "Speaking (normal speech)", 'value': 72},
+    {'label': "Speaking (voiced counting)", 'value': 72},
+    {'label': "Speaking (loud speech)", 'value': 142},
+    {'label': "Singing (whispered 'aah')", 'value': 103},
+    {'label': "Singing (voiced 'aah')", 'value': 970},
+]
+
+def_rt = 0.2 / 10  # default risk tolerance
+age_levels = [
+    {'label': "Ages 0-4", 'value': def_rt / 0.025},
+    {'label': "Ages 5-17", 'value': def_rt / 0.008},
+    {'label': "Ages 18-49", 'value': def_rt / 0.2},
+    {'label': "Ages 50-64", 'value': def_rt / 0.61},
+    {'label': "Ages 75-84", 'value': def_rt / 1.30},
+    {'label': "Ages >85", 'value': def_rt / 1.45},
+]
+
+mask_types = [
+    {'label': "None", 'value': 1},
+    {'label': "Cloth", 'value': 0.15},
+    {'label': "N95 Surgical", 'value': 0.05},
+]
+
+app.layout = html.Div(children=[
+    html.H1(children='COVID-19 Indoor Safety Guidelines'),
+
+    html.Div(children='''
+        Reference: Martin Z. Bazant and John W. M. Bush, medRxiv preprint (2020):
+        "Beyond Six Feet: A Guideline to Limit Indoor Airborne Transmission of COVID-19"
+    '''),
+
+    html.Br(),
+
+    dcc.Graph(
+        id='safety-graph',
+        figure=fig
+    ),
+
+    html.Div([
+        html.Div([
+            html.H6("Room Specifications: "),
+            html.Br(),
+            html.Div(["Floor Area (sq. ft.): ",
+                      dcc.Input(id='floor-area', value=900, type='number')]),
+            html.Br(),
+            html.Div(["Ceiling Height (ft.): ",
+                      dcc.Input(id='ceiling-height', value=12, type='number')]),
+            html.Br(),
+            html.Div(["Ventilation System: ",
+                      dcc.Dropdown(id='ventilation-type',
+                                   options=ventilation_types,
+                                   value=0.34)]),
+            html.Br(),
+            html.Div(["Filtration System: ",
+                      dcc.Dropdown(id='filter-type',
+                                   options=filter_types,
+                                   value=0)]),
+            html.Br(),
+            html.Div(["Outdoor Air Fraction: ",
+                      dcc.Slider(id='outdoor-air-fraction',
+                                 min=0.01,
+                                 max=1,
+                                 step=0.01,
+                                 value=0.2,
+                                 marks={
+                                     0.01: {'label': '0 (closed room)'},
+                                     1: {'label': '1 (outdoors)'}
+                                 })])
+        ], style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            html.H6("Human Behavior: "),
+            html.Br(),
+            html.Div(["Exertion Level: ",
+                      dcc.Dropdown(id='exertion-level',
+                                   options=exertion_types,
+                                   value=0.49)]),
+            html.Br(),
+            html.Div(["Expiratory Activity: ",
+                      dcc.Dropdown(id='exp-activity',
+                                   options=expiratory_types,
+                                   value=29)]),
+            html.Br(),
+            html.Div(["Age Group: ",
+                      dcc.Dropdown(id='risk-tolerance',
+                                   options=age_levels,
+                                   value=def_rt / 0.2)]),
+            html.Br(),
+            html.Div(["Masks? ",
+                      dcc.Dropdown(id='mask-type',
+                                   options=mask_types,
+                                   value=0.15)])
+        ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
+        html.Br()
+    ])
+])
+
+
+@app.callback(
+    Output('safety-graph', 'figure'),
+    [Input('floor-area', 'value'),
+     Input('ceiling-height', 'value'),
+     Input('ventilation-type', 'value'),
+     Input('outdoor-air-fraction', 'value'),
+     Input('filter-type', 'value'),
+     Input('exertion-level', 'value'),
+     Input('exp-activity', 'value'),
+     Input('mask-type', 'value'),
+     Input('risk-tolerance', 'value')]
+)
+def update_figure(floor_area, ceiling_height, air_exchange_rate, outdoor_air_fraction, aerosol_filter_eff,
+                  breathing_flow_rate, infectiousness, mask_passage_prob, risk_tolerance):
+    myInd.physical_params = [floor_area, ceiling_height, air_exchange_rate, outdoor_air_fraction,
+                             aerosol_filter_eff]
+    myInd.physio_params = [breathing_flow_rate, 2]
+    myInd.disease_params = [infectiousness, 0.3]
+    myInd.prec_params = [mask_passage_prob, risk_tolerance]
+    new_df = myInd.calc_n_max_series(2, 100, 1.0)
+    new_fig = px.line(new_df, x="Maximum Exposure Time (hours)", y="Maximum Occupancy",
+                      title="Maximum Occupancy & Exposure Time")
+    new_fig.update_layout(transition_duration=500)
+
+    return new_fig
+
+
+if __name__ == "__main__":
+    app.run_server()
