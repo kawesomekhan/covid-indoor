1acb4e20232cb15f20f6f285e660aa9e984d4b21
kawesomekhan <kasimkhan98@gmail.com>
2020-12-02 18:00:11 -0600

    Added Talisman

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+from flask_talisman import Talisman
 
 """
 app.py is to be imported by other files (default.py, advanced.py, index.py) to be referenced in setup.
@@ -7,5 +8,7 @@ app.py is to be imported by other files (default.py, advanced.py, index.py) to b
 
 # Dash App Setup
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
+
+Talisman(app.server, force_https=True)
 
 curr_units = 'british'
