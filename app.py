75c5c6bd5a2baa0a81c6024d37444de283c4b4e5
cheeriodp <hey_caeli@msn.com>
2021-03-07 19:28:15 -0800

    second test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_talisman import Talisman
 app.py is to be imported by other files (default.py, advanced.py, index.py) to be referenced in setup.
 
 """
-# Test push to branch
+# Test push to branch again
 # Dash App Setup
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
